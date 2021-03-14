b59b0893f7ca3639eb9e40a3f149709c5b7b1371
KamilWyszynski1 <kamil.wyszynski.97@gmail.com>
2021-03-14 22:52:26 +0100

    refactor

--- dWill/contracts/Will.sol
+++ dWill/contracts/Will.sol
@@ -1,44 +1,49 @@
 pragma solidity 0.8.2;
 
 contract Will {
-    uint256 public ethStorage;
+    /*
+    Concept:
+        * someone wants to create will and deposit some amount of money
+        * person does that and he'll be automatically emailed from time to time
+          to ping contract in order to not allow withdrawing money before time
+        * if person that deposited money won't ping smart contract (e.g. person is dead)
+          withdrawal become possible and 
 
-    function store(uint256 _amount) external returns (bool) {
-        ethStorage += _amount;
-        return true;
-    }
+    */
 
     event Deposited(address indexed payee, uint256 weiAmount);
     event Withdrawn(address indexed payee, uint256 weiAmount);
 
     mapping(address => uint256) private _deposits;
 
-    function depositsOf(address payee) public view returns (uint256) {
-        return _deposits[payee];
+    function depositsOf(address _payee) public view returns (uint256) {
+        return _deposits[_payee];
     }
 
     /**
      * @dev Stores the sent amount as credit to be withdrawn.
-     * @param payee The destination address of the funds.
+     * @param _payee The destination address of the funds.
      */
-    function deposit(address payee) public payable {
+    function deposit(address _payee) public payable {
         uint256 amount = msg.value;
-        _deposits[payee] = _deposits[payee] + amount;
+        _deposits[_payee] = _deposits[_payee] + amount;
 
-        emit Deposited(payee, amount);
+        emit Deposited(_payee, amount);
     }
 
     /**
-     * @dev Withdraw accumulated balance for a payee.
-     * @param payee The address whose funds will be withdrawn and transferred to.
+     * @dev Withdraw accumulated balance for a _payee.
+     * @param _payee The address whose funds will be withdrawn and transferred to.
      */
-    function withdraw(address payable payee) public {
-        uint256 payment = _deposits[payee];
+    function withdraw(address payable _payee) public {
+        require(_payee == msg.sender, "Only owner can withdraw money");
 
-        _deposits[payee] = 0;
+        uint256 payment = _deposits[_payee];
 
-        payee.transfer(payment);
+        _deposits[_payee] = 0;
 
-        emit Withdrawn(payee, payment);
+        _payee.transfer(payment);
+
+        emit Withdrawn(_payee, payment);
     }
 }
